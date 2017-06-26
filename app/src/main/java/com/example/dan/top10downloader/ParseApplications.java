9db8f387b8b1b39f3e3f06e23d057241ebd499da
Dan Hoying <danhoying@gmail.com>
2017-06-26 08:40:44 -0700

    Add parsing loop

--- app/src/main/java/com/example/dan/top10downloader/ParseApplications.java
+++ app/src/main/java/com/example/dan/top10downloader/ParseApplications.java
@@ -1,5 +1,7 @@
 package com.example.dan.top10downloader;
 
+import android.util.Log;
+
 import org.xmlpull.v1.XmlPullParser;
 import org.xmlpull.v1.XmlPullParserFactory;
 
@@ -35,7 +37,42 @@ public class ParseApplications {
             xpp.setInput(new StringReader(xmlData));
             int eventType = xpp.getEventType();
             while (eventType != XmlPullParser.END_DOCUMENT) {
+                String tagName = xpp.getName();
+                switch (eventType) {
+                    case XmlPullParser.START_TAG:
+                        Log.d(TAG, "parse: Starting tag for " + tagName);
+                        if ("entry".equalsIgnoreCase(tagName)) {
+                            inEntry = true;
+                            currentRecord = new FeedEntry();
+                        }
+                        break;
+                    case XmlPullParser.TEXT:
+                        textValue = xpp.getText();
+                        break;
+                    case XmlPullParser.END_TAG:
+                        Log.d(TAG, "parse: Ending tag for " + tagName);
+                        if (inEntry) {
+                            if ("entry".equalsIgnoreCase(tagName)) {
+                                applications.add(currentRecord);
+                                inEntry = false;
+                            } else if ("name".equalsIgnoreCase(tagName)) {
+                                currentRecord.setName(textValue);
+                            } else if ("artist".equalsIgnoreCase(tagName)) {
+                                currentRecord.setArtist(textValue);
+                            } else if ("releaseDate".equalsIgnoreCase(tagName)) {
+                                currentRecord.setReleaseDate(textValue);
+                            } else if ("summary".equalsIgnoreCase(tagName)) {
+                                currentRecord.setSummary(textValue);
+                            } else if ("image".equalsIgnoreCase(tagName)) {
+                                currentRecord.setImageURL(textValue);
+                            }
+                        }
+                        break;
 
+                    default:
+                        // Nothing else to do.
+                }
+                eventType = xpp.next();
             }
 
         } catch (Exception e) {
